56b61310c8983534ef314ea98679477cdd998964
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-04-26 15:10:32 -0300

    Desafio concluído

---
+++ Respostas.sql
@@ -0,0 +1,97 @@
+-- 1
+SELECT
+	Nome,
+	Ano
+FROM Filmes;
+
+-- 2
+SELECT
+	Nome,
+	Ano
+FROM Filmes
+ORDER BY Ano;
+
+-- 3
+SELECT
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Nome = 'De Volta para o Futuro';
+
+-- 4
+SELECT
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Ano = 1997;
+
+-- 5
+SELECT
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Ano > 2000;
+
+-- 6
+SELECT
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE
+	Duracao > 100
+	AND Duracao < 150
+ORDER BY Duracao;
+
+-- 7
+SELECT
+	Ano,
+	COUNT(*) AS Quantidade
+FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC;
+
+-- 8
+SELECT
+	PrimeiroNome,
+	UltimoNome
+FROM Atores
+WHERE Genero = 'M';
+
+-- 9
+SELECT
+	PrimeiroNome,
+	UltimoNome
+FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome;
+
+-- 10
+SELECT
+	f.Nome,
+	g.Genero
+FROM Filmes f
+JOIN FilmesGenero fg ON f.Id = fg.IdFilme
+JOIN Generos g ON fg.IdGenero = g.Id;
+
+-- 11
+SELECT
+	f.Nome,
+	g.Genero
+FROM Filmes f
+JOIN FilmesGenero fg ON f.Id = fg.IdFilme
+JOIN Generos g ON fg.IdGenero = g.Id
+WHERE g.Genero = 'Mistério';
+
+-- 12
+SELECT
+	f.Nome,
+	a.PrimeiroNome,
+	a.UltimoNome,
+	ef.Papel
+FROM Filmes f
+JOIN ElencoFilme ef ON f.Id = ef.IdFilme
+JOIN Atores a ON ef.IdAtor = a.Id
